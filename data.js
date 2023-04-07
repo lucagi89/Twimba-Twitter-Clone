3f1ae3838d34061f4bf3278f0c325cf118b6f6ba
lucagi89 <106192483+lucagi89@users.noreply.github.com>
2023-04-07 01:22:12 +0100

    a twitter clone, to practice

---
+++ data.js
@@ -0,0 +1,57 @@
+export const tweetsData = [   
+    {
+        handle: `@Nobody34567 👑`,
+        profilePic: `images/troll.jpg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Everyone is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid.`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@RealDonaldTrump 🇺🇸`,
+        profilePic: `images/musk.png`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `I am the best president in the world!`,
+        replies: [
+                  {
+                handle: `@TomCruise ✅`,
+                profilePic: `images/tcruise.png`,
+                tweetText: `You're not even a president anymore... 😎🛩`,
+            },
+                  {
+                handle: `@ChuckNorris ✅`,
+                profilePic: `images/chucknorris.jpeg`,
+                tweetText: `Good, are you done cleaning my shoes!🫵🏻`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@Anonymous 🕵️‍♂️`,
+        profilePic: `images/flower.png`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `We are legion. We do not forgive. We do not forget. Expect us.`,
+        replies: [
+            {
+                handle: `@StackOverflower ☣️`,
+                profilePic: `images/overflow.png`,
+                tweetText: `No. Obviosuly not. Go get a job in McDonald's.`,
+            },
+            {
+                handle: `@YummyCoder64`,
+                profilePic: `images/love.png`,
+                tweetText: `Love us ❤️`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
